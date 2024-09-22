c9fbc867718c4f88c351b173eb3654496e340fdf
MrcsFranca <francamarcos035@gmail.com>
2024-09-22 10:04:29 -0300

    Enhacing number of data and bubbleSort

--- mips-factorial.asm
+++ mips-factorial.asm
@@ -1,5 +1,5 @@
 .data
-	quebraLinha: .asciiz "\n"
+	newLine: .asciiz "\n"
 
 .text
 	j main
@@ -15,100 +15,82 @@
 		j for
 		continua:
 			jr $ra
-		
-	
-	ordenacao:
-		move $t3, $t0
-		move $t4, $t1
-		move $t5, $t2
-	
-		li $s1, 0
-	
-		bubbleSort:
-			bgt $t5, $t3, ord1
-			j keepGoing
-	
-			ord1:
-				move $t6, $t5 #t6 eh o aux
-				move $t5, $t3 
-				move $t3, $t6 
-			keepGoing:
-	
-			bgt $t5, $t4, ord2
-			j keepGoing2
-	
-			ord2:
-				move $t6, $t5 #t6 eh o aux
-				move $t5, $t4 
-				move $t4, $t6 
-			keepGoing2:
 			
-			bgt $t4, $t3, ord3
-			j keepGoing3
+	bubbleSort:
+		move $t2, $sp # $t2 recebe o topo da minha pilha
+		li $t1, 1 # j = 1 -> vai rodar até eu ter certeza que todos os números foram comparados
+		loopAteSemTroca:
+			beq $t1, $t0, endBubbleSort # se analisou todos os números
 			
-			ord3:
-				move $t6, $t4
-				move $t4, $t3
-				move $t3, $t6
-			keepGoing3:
-			
-	
-		addi $s1, $s1, 1
-		beq $s1, 2, bubbleSort
+			loopDeComparacao:
+				
+				lw $t3, 0($t2) # valor do topo
+				lw $t4, 4($t2) # segundo valor do topo: como se fosse i + 1
+				ble $t3, $t4, noSwap # se valor do topo for menor ou igual ao próximo valor, não precisa trocar
+					sw $t3, 4($t2)
+					sw $t4, 0($t2)
+			j loopDeComparacao 
+			noSwap:
+				addi $t2, $t2, 4 # atual = atual + 1
+				addi $t1, $t1, 1
+				j loopAteSemTroca
+	endBubbleSort:
+		addi $t5, $t5, 1
+		blt $t5, $t0, bubbleSort
 		jr $ra
-		
-		
 	
 	main:
 		#leitura de num
-		while1:
-			li $v0, 5
+		
+		whileLength:
+			li $v0, 5 #t0 tem a quantidade de números que quero ler
 			syscall
 			move $t0, $v0
-		bltz $t0, while1
-		move $s0, $t0
-		jal fatorial
-		move $t0, $s1
+		ble $t0, 0, whileLength #só aceita quantidades maior que 0
 		
+		li $t1, 0
+		
+		readValue:
+			beq $t1, $t0, endReader
+			
+				whileValue:
+					li $v0, 5
+					syscall
+					move $s0, $v0
+				bltz $s0, whileValue #só fatorial maior ou igual a zero
+			jal fatorial
+			move $v0, $s1
+			
+			sw $s1, 0($sp) #armazena valor na stack
+			
+			addi $sp, $sp, -4 #muda posição na stack
+			
+			addi $t1, $t1, 1
+		j readValue
+		endReader:
 	
-		while2:
-			li $v0, 5
+		addi $sp, $sp, 4
+		li $t5, 0 #i = 0
+		jal bubbleSort
+		
+		move $t1, $sp
+		li $t2, 0
+		print:
+		beq $t2, $t0, EOF
+			lw $t3, 0($t1)
+			li $v0, 1
+			move $a0, $t3
 			syscall
-			move $t1, $v0
-		blt $t1, 0, while2
-		move $s0, $t1
-		jal fatorial
-		move $t1, $s1
-	
-	
-		while3:
-			li $v0, 5
+			
+			li $v0, 4
+			la $a0, newLine
 			syscall
-			move $t2, $v0
-		blt $t2, 0, while3
-		move $s0, $t2
-		jal fatorial
-		move $t2, $s1
-	
+			
+			addi $t1, $t1, 4
+			
+		addi $t2, $t2, 1
+		j print
 		
-		jal ordenacao
-		
-		li $v0, 1
-		move $a0, $t3
-		syscall
-		
-		li $v0, 4
-		la $a0, quebraLinha
-		syscall
-		
-		li $v0, 1
-		move $a0, $t4
-		syscall
-		
-		li $v0, 4
-		la $a0, quebraLinha
-		syscall
-		
-		li $v0, 1
-		move $a0, $t5
+		EOF:
+		li $v0, 10
 		syscall
